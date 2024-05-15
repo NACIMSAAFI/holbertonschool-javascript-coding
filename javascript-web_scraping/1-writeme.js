c0cf81ff147f5785b44c71361d251136abdc8066
NACIMSAAFI <7715@holbertonstudents.com>
2024-05-15 21:51:18 +0100

    Add 1-writeme.js

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 const filePath = process.argv[2];
 
-buffer = process.argv[3]
+buffer = process.argv[3];
 
 fs.writeFile(filePath, buffer, 'utf-8', (err, data) => {
   if (err) {
