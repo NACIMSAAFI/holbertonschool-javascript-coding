05f86bfd1b518494675a8d95289b1e449afa6661
NACIMSAAFI <7715@holbertonstudents.com>
2024-05-17 18:49:06 +0100

    Add 4-starwars_count.js

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 
-request(process.argv[2], (err, response, body) => {
+request(process.argv[2], function(err, response, body) {
   if (err) {
     console.error('Error:', err);
   } else {
