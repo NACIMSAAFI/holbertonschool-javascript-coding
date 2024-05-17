918afed18fd8ea72b042351d7c51d158de7642e4
NACIMSAAFI <7715@holbertonstudents.com>
2024-05-17 18:54:53 +0100

    Add 4-starwars_count.js

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,12 +1,20 @@
 #!/usr/bin/node
+
 const request = require('request');
-request(process.argv[2], function (error, response, body) {
-  if (!error) {
+
+const url = process.argv[2];
+
+request(url, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
     const results = JSON.parse(body).results;
-    console.log(results.reduce((count, movie) => {
-      return movie.characters.find((character) => character.endsWith('/18/'))
-        ? count + 1
-        : count;
-    }, 0));
+    const count = results.reduce((acc, movie) => {
+      if (movie.characters.some((character) => character.endsWith('/18/'))) {
+        acc++;
+      }
+      return acc;
+    }, 0);
+    console.log(count);
+  } else {
+    console.error('Error:', error);
   }
 });
