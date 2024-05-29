f7bb28a11f3148a179793cb8cdb43a5b65526a26
NACIMSAAFI <7715@holbertonstudents.com>
2024-05-29 21:54:45 +0100

    Add 2-read_file.js

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+
+    if (lines.length <= 1) {
+      throw new Error('Cannot load the database');
+    }
+
+    const students = {};
+    const headers = lines[0].split(',');
+
+    for (let i = 1; i < lines.length; i += 1) {
+      const studentData = lines[i].split(',');
+      if (studentData.length === headers.length) {
+        const field = studentData[3];
+        if (!students[field]) {
+          students[field] = [];
+        }
+        students[field].push(studentData[0]);
+      }
+    }
+
+    const totalStudents = Object.values(students).reduce((acc, arr) => acc + arr.length, 0);
+    console.log(`Number of students: ${totalStudents}`);
+
+    Object.keys(students).forEach((field) => {
+      const list = students[field].join(', ');
+      console.log(`Number of students in ${field}: ${students[field].length}. List: ${list}`);
+    });
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
